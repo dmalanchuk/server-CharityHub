6e73e5d88017d834a14be5a9adae6adc38085def
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-25 21:07:50 +0300

    starting write repository for categories_api.py

--- src/repositories/categories_repo.py
+++ src/repositories/categories_repo.py
@@ -1,9 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_session
+from src.schemas.categories_schema import CategoryBase
 
 
 class CategoriesRepository:
 
     @staticmethod
-    async def get_session():
+    async def get_all() -> list[CategoryBase]:
+        ...
+
+
+    @staticmethod
+    async def create() -> CategoryBase:
         ...
