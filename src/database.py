0d3e300a54d82807c4060db8e980ba263740008c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-26 17:36:21 +0300

    remove a @asynccontexmanager

--- src/database.py
+++ src/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
 
@@ -19,7 +18,6 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 Base = declarative_base()
 
 
-@asynccontextmanager
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
