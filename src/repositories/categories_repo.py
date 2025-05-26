803251de49b5dbb2e0a05f21d632cd06c2ecde42
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-26 15:50:38 +0300

    wtited repo for categories

--- src/repositories/categories_repo.py
+++ src/repositories/categories_repo.py
@@ -1,15 +1,21 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.database import get_session
+from src.models.categories_model import CategoriesModel
 from src.schemas.categories_schema import CategoryBase
 
 
 class CategoriesRepository:
 
     @staticmethod
-    async def get_all() -> list[CategoryBase]:
-        ...
+    async def get_all(session: AsyncSession):
+        result = await session.execute(select(CategoriesModel))
+        return result.scalars().all()
 
 
     @staticmethod
-    async def create() -> CategoryBase:
-        ...
+    async def create(session: AsyncSession, data: CategoryBase):
+        category = CategoriesModel(**data.to_dict())
+        data.session.add(category)
+        await data.session.commit()
+        await data.session.refresh(category)
+        return category
