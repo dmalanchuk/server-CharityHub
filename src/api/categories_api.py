f87d1aca9d7e49c23d9e2c41f8e75a64807137eb
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-26 17:38:51 +0300

    writed api for categories and delete all bugs

--- src/api/categories_api.py
+++ src/api/categories_api.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_session
+from src.repositories.categories_repo import CategoriesRepository
+from src.services.categories_service import CategoriesService
 from src.schemas.categories_schema import CategoryBase
 
 categories_router = APIRouter(
@@ -9,13 +14,14 @@ categories_router = APIRouter(
 
 @categories_router.get("/")
 async def get_categories(
-
-) -> list[CategoryBase]:
-    ...
+    session: AsyncSession = Depends(get_session)
+):
+    return await CategoriesRepository.get_all(session)
 
 
 @categories_router.post("/")
 async def post_categories(
-        category: CategoryBase
+        data: CategoryBase,
+        session: AsyncSession = Depends(get_session)
 ):
-   ...
+   return await CategoriesService.create_category(session, data)
