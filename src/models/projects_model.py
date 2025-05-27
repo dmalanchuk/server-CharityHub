987533a698417e35e9cd53b2225242adc0400615
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-27 17:07:34 +0300

    upgrade model projects added column 'url_donations' and renamed column 'owner_id' on 'user_id'

--- src/models/projects_model.py
+++ src/models/projects_model.py
@@ -8,12 +8,13 @@ from src.database import Base
 class ProjectsModel(Base):
     __tablename__ = "projects"
 
-    id: Mapped[int] = mapped_column("id", primary_key=True)
-    title: Mapped[str] = mapped_column("title", nullable=False)
-    description: Mapped[str] = mapped_column("description", nullable=False)
-    goal_amount: Mapped[int] = mapped_column("goal_amount", nullable=False)
-    current_amount: Mapped[int] = mapped_column("current_amount", nullable=False)
-    category_id: Mapped[int] = mapped_column("category_id", ForeignKey("categories.id"))
-    owner_id: Mapped[int] = mapped_column("user_id", ForeignKey("users.id"))
-    start_date: Mapped[datetime.datetime] = mapped_column("start_date")
-    end_date: Mapped[datetime.datetime] = mapped_column("end_date")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    goal_amount: Mapped[int] = mapped_column(nullable=False)
+    current_amount: Mapped[int] = mapped_column(nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    start_date: Mapped[datetime.datetime] = mapped_column()
+    end_date: Mapped[datetime.datetime] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    url_donations: Mapped[str] = mapped_column()
