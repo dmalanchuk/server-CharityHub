66d592c7506ed17fd9825600a56123fca72ee811
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-28 16:38:34 +0300

    create two post request

--- src/api/auth_api.py
+++ src/api/auth_api.py
@@ -2,5 +2,16 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-auth_router = APIRouter()
+auth_router = APIRouter(
+    prefix="/auth"
+)
 
+
+@auth_router.post("/login")
+async def login_users():
+    ...
+
+
+@auth_router.post("/register")
+async def register_users():
+    ...
