8f2ffa7036283ce33f64554abfc444804707fec4
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-28 16:31:45 +0300

    added new router auth api

--- src/api/router.py
+++ src/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from src.api import categories_api
+from src.api import categories_api, auth_api
 
 router = APIRouter()
 router.include_router(categories_api.categories_router, tags=["Categories"])
+router.include_router(auth_api.auth_router, tags=["Auth"])
