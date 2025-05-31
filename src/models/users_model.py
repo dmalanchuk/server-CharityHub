9dbe68eaf948b33b0bf548b880d755f3aa413dd8
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-05-31 21:01:34 +0300

    rewrited all migrations and model sql

--- src/models/users_model.py
+++ src/models/users_model.py
@@ -1,11 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from src.database import Base
-
-
-class UserModel(Base):
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column("id", primary_key=True)
-    name: Mapped[str] = mapped_column("name", nullable=False)
-    email: Mapped[str] = mapped_column("email", nullable=False)
-    email_code: Mapped[int] = mapped_column("email_code", nullable=False)
+# from sqlalchemy.orm import Mapped, mapped_column
+# from src.database import Base
+#
+#
+# class UserModel(Base):
+#     __tablename__ = "users"
+#
+#     id: Mapped[int] = mapped_column("id", primary_key=True)
+#     name: Mapped[str] = mapped_column("name", nullable=False)
+#     email: Mapped[str] = mapped_column("email", nullable=False)
+#     email_code: Mapped[int] = mapped_column("email_code", nullable=False)
