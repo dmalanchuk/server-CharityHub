8ba2b4b04a72135703d20f188ec55886a46c90d3
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-03 22:59:56 +0300

    setting email_utils.py on sending fake list

--- src/core/email_utils.py
+++ src/core/email_utils.py
@@ -1,15 +1,29 @@
 from email.message import EmailMessage
+from aiosmtplib import SMTP
 
-import aiosmtplib
 
-async def send_email(from_email: str, to_email: str, subject: str, body: str):
+SMTP_HOST = "sandbox.smtp.mailtrap.io"
+SMTP_PORT = 587
+SMTP_USER = "95a1307be86e18"
+SMTP_PASS = "7de596792ef732"
 
+
+async def send_verification_email(code: str, to_email: str):
     msg = EmailMessage()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-    msg.set_content(body)
+    msg["FROM"] = "noreply@charityhub.com"
+    msg["TO"] = to_email
+    msg["SUBJECT"] = "Verify your email"
+    msg.set_content(f"Your verification code is: {code}")
 
-    await aiosmtplib.send(
-        ...
+    smtp = SMTP(
+        hostname=SMTP_HOST,
+        port=SMTP_PORT,
+        use_tls=False
     )
+
+    await smtp.connect()
+    await smtp.starttls()
+    await smtp.login(SMTP_USER, SMTP_PASS)
+    await smtp.send_message(msg)
+    await smtp.quit()
+
