7cab0e05cb8a4d021857995fb4c1cfe7a75ac4ba
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-04 20:23:06 +0300

    modify model - remove nullable in verification_code, and modify schemas

--- src/schemas/users_schema.py
+++ src/schemas/users_schema.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class User(BaseModel):
     username: str
     email: EmailStr
 
-    class Config:
-        orm_mode = True
-
 
 class GetInfoUser(User):
     id: int
     is_verified: bool
 
+    class Config:
+        orm_mode = True
+
 
 class CreateUser(User):
-    password: str
-    verified_code: str
+    password: str = Field(min_length=8)
+    verification_code: str
