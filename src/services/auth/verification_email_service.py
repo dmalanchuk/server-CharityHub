b489290dc2c49af79f70e9d3be521023398af9bb
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-15 00:12:36 +0300

    update

---
+++ src/services/auth/verification_email_service.py
@@ -0,0 +1,19 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repositories.auth.get_verify_code_repo import GetVerifyCodeRepo
+from src.models.users_model import UserModel
+
+class VerificationEmailService:
+
+    @staticmethod
+    async def verification_email_service(ver_code: str, session: AsyncSession):
+        saved_code = await GetVerifyCodeRepo.get_verification_code_db(ver_code, session)
+
+        if saved_code and saved_code.strip() == ver_code.strip():
+            await session.execute(
+                UserModel.__table__.update().where(UserModel.verified_code == ver_code).values(is_verified=True)
+            )
+            await session.commit()
+        else:
+            raise HTTPException(status_code=404, detail="Verification code is incorrect")
