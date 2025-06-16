45cd9b39825e03601f40fbba3b3a32217491b811
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-17 00:18:47 +0300

    create new post api

--- src/api/projects_api.py
+++ src/api/projects_api.py
@@ -1,4 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.schemas.projects_schema import CreateProject
+from src.services.projects.projects_service import ProjectsService
 from src.database import get_session
 
 from fastapi import APIRouter, Depends
@@ -8,3 +11,10 @@ from typing import Annotated
 projects_router = APIRouter(
     prefix="/projects"
 )
+
+@projects_router.post("/create/project")
+async def create_project(
+    data: Annotated[CreateProject, Depends()],
+    session: AsyncSession = Depends(get_session)
+):
+    return await ProjectsService.create_project(data ,session)
