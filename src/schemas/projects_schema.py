83212817a6314ee028cbdd0e81a6311fb043bc0a
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-17 00:19:17 +0300

    update

--- src/schemas/projects_schema.py
+++ src/schemas/projects_schema.py
@@ -1,9 +1,13 @@
+import datetime
+
 from pydantic import BaseModel
 
 
 class CreateProject(BaseModel):
     title: str
     description: str
-    category: str
+    category_id: int
+    owner: str
     goal_amount: int
     url_donations: str
+    end_date: datetime.datetime
