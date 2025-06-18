d7a104023229a867dbf9c9bb01a4af0284ca0937
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-18 14:45:02 +0300

    added config

--- src/schemas/projects_schema.py
+++ src/schemas/projects_schema.py
@@ -6,8 +6,11 @@ from pydantic import BaseModel
 class CreateProject(BaseModel):
     title: str
     description: str
-    category_id: int
-    owner: str
     goal_amount: int
-    url_donations: str
+    iban_details: str
     end_date: datetime.datetime
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.strftime('%d.%m.%Y')
+        }
