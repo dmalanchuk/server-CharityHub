e0704ee23dcb686237b89ca4d63d9d538621b099
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-18 13:00:16 +0300

    added middleware cors settings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
 from src.api import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(router.router)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
