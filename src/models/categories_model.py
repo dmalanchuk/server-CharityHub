28256443b6d523787ea561256ef53e30f375f70e
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-23 14:30:28 +0300

    del ful categories

--- src/models/categories_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from src.database import Base
-
-
-class CategoriesModel(Base):
-    __tablename__ = "categories"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column()
